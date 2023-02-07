0b6f17db4bfdc07d31d76a99acca4d07e9f4fefd
pillar <ymblender@gmail.com>
2023-02-07 23:55:27 +0800

    feat: create twoSum property

--- src/__test__/twoSum.spec.ts
+++ src/__test__/twoSum.spec.ts
@@ -2,18 +2,48 @@ import fc from "fast-check";
 import { describe, expect, it } from "vitest";
 import { twoSum } from "../twoSum";
 
-describe("use fast-check", () => {
+const realTwoSum = (list: number[], target: number) => {
+  const cache: Record<number, number> = {};
+  for (let i = 0; i < list.length; i++) {
+    const v = list[i];
+    if (cache[target - v] !== void 0) {
+      return [i, cache[target - v]];
+    }
+    cache[v] = i;
+  }
+  return [];
+};
+
+describe("how to create success test property", () => {
   it("two sum", () => {
     fc.assert(
       fc.property(
-        fc
-          .array(fc.integer().filter((v) => v >= 0))
-          .filter((v) => v[0] + v[1] === 10 && !!v[4]),
-        fc.integer().filter((v) => v === 10),
-        (list, target) => {
+        // create some number[], the list length / 2 and length / 3 is sum of length * 2
+        fc.array(fc.integer().filter((v) => v >= 0)).filter((list) => {
+          // need has success result
+          if (list.length < 2) {
+            return false;
+          }
+          const a = ~~(list.length / 2);
+          const b = ~~(list.length / 3);
+          const target = list.length * 2;
+          if (a === b) {
+            return false;
+          }
+
+          // get success indexs
+          const result = realTwoSum(list, target);
+          return (
+            (result[0] === a && result[1] === b) ||
+            (result[1] === a && result[0] === b)
+          );
+        }),
+        (list) => {
+          const target = list.length * 2;
+
           expect(twoSum(list, target)).deep.oneOf([
-            [0, 1],
-            [1, 0],
+            [~~(list.length / 2), ~~(list.length / 3)],
+            [~~(list.length / 2), ~~(list.length / 3)].reverse(),
           ]);
         }
       ),
